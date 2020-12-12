7eaf69938a5993ebae400f880653fcd99e770e94
Jóhann Sveinn Ingason <yohans89@gmail.com>
2020-12-12 12:34:15 +0000

    Saving work before building dist

---
+++ server.js
@@ -0,0 +1,20 @@
+(() => {
+	"use strict";
+	const express = require("express");
+	const http = require("http");
+	const cors = require("cors");
+	const app = express();
+	const server = http.createServer(app);
+
+	app.use(cors());
+
+	app.get("/", function (req, res) {
+		res.send("Hello world!");
+	});
+
+	server.listen(9001, function () {
+		console.log("Express server listening on port " + server.address().port);
+	});
+	
+	module.exports = app;
+})();
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import { api } from "./api";
+
+const App = () => {
+  const [successText, setSuccessText] = useState(null);
+
+  useEffect(() => {
+    api
+      .get("/")
+      .then(({ data }) => setSuccessText(data))
+      .catch((err) => console.error(err));
+  });
+
+  return (
+    <div>
+      <h2>Hello World, Ernir is running!</h2>
+      <p>Fetched api response from server: {successText}</p>
+    </div>
+  );
+};
+
+export default App;
